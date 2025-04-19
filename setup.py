13a014721e39da9cb786964406e56968b55cf186
mallen012 <46409832+mallen012@users.noreply.github.com>
2025-04-18 21:52:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author="Mike Allen",
     author_email="your@email.com",
     description="Custom Webex Bot Framework with WebSocket Fix",
-    long_description=open("README.md").read(),
+    long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/mallen012/webex_bot",
     classifiers=[
